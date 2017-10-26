e8a1772d008044193b934054bd74d83e097301cf
Tara Meyerink <tarajmeyerink@gmail.com>
2017-10-26 17:53:13 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,19 @@
 class dog
 
-def name=(dogs_name)
-  @name = dogs_name
-end
+  def name=(dogs_name)
+    @name = dogs_name
+  end
 
-def name
-  @name
-end
+  def name
+    @name
+  end
 
-def breed
-  @breed
-end
+  def breed
+    @breed
+  end
 
-def breed=(dogs_breed)
-  @breed = dogs_breed
-end
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+  end
 
 end
